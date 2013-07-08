9dcc6a209830cba9454d219064da9ebf2a9132bb
Chris Boursnell <cmb211@cam.ac.uk>
2013-07-08 16:48:42 +0100

    Added memory requirement options

--- khmer-batch.rb
+++ khmer-batch.rb
@@ -23,6 +23,9 @@ opts = Trollop::options do
   opt :input, "A file of fastq files, 1 per line", :type => String
   opt :files, "A list of colon separated input fastq files", :type => String
   opt :script, "Specify the location of the khmer normalize-by-median.py script if it is not in your PATH", :default => "normalize-by-median.py", :type => String
+  opt :memory, "Maximum amount of memory to be used by khmer in gigabytes (default:4)", :default => 4.0, :type => :float
+  opt :kmer, "K value to use in khmer (default:21)", :default => 21, :type => :int
+  opt :buckets, "Number of buckets (default:4)", :default => 4, :type => :int
 end
 
 filelist=[]
@@ -40,11 +43,23 @@ elsif opts.files
   filelist = opts.files.split(":")
   filelist.map! { |file|  File.expand_path(file)}
   filelist.each do |file|
-    puts file
     if !File.exists?(file)
       abort "Can't find file \"#{file}\""
-    else
-      puts "Found \"#{file}\""
     end
   end
 end
+
+# normalize-by-median.py -p -k 20 -N 12 -x 64e9 --savehash table.kh filename.fq
+# normalize-by-median.py -p -k 20 -N 12 -x 64e9 --loadhash table.kh --savehash table2.kh filename2.fq
+
+#For normalize-by-median, khmer uses one byte per hash entry, 
+#  so: if you had 16 GB of available RAM, you should specify 
+#  something like -N 4 -x 4e9, which multiplies out to about 16 GB.
+
+puts "#{opts.memory}GB"
+
+n = opts.buckets
+x = opts.memory/opts.buckets*1e9
+
+puts "Number of buckets to use is #{n}"
+puts "Size of each bucket is #{x.to_i}"
