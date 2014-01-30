371411dda5a4c3e3702298267443723d2c1a17d9
Chris Boursnell <cmb211@cam.ac.uk>
2014-01-30 16:52:06 +0000

    fixed substr range; added -o option

--- extract_fasta_from_gff.rb
+++ extract_fasta_from_gff.rb
@@ -16,6 +16,7 @@ Chris Boursnell (cmb211@cam.ac.uk)
 EOS
   opt :genome, "Genome", :required => true, :type => String
   opt :annotation, "Gff file", :required => true, :type => String
+  opt :output, "Output file (defaults to stdout)", :type => String
   opt :verbose, "Be verbose"
 end
 
@@ -39,8 +40,8 @@ class Gff
     end
     @chromosome = a[0]
     @type = a[2]
-    @start = a[3].to_i
-    @stop = a[4].to_i
+    @start = a[3].to_i - 1
+    @stop = a[4].to_i - 1
     @id = name
     @strand = a[6]
   end
@@ -70,33 +71,30 @@ File.open("#{opts.annotation}", "r").each_line do |line|
   end
 end
 
+out = File.open("#{opts.output}", "w") if opts.output
+
 hash.each_pair do |id, list|
-  if list[0].strand == "+"
-    list.sort! { |x,y| x.start <=> y.start }
-    seq=""
-    if genome_hash.has_key?(list[0].chromosome)
-      list.each do |i|
-        s = genome_hash[i.chromosome]
-        # puts "substring #{i.start} to #{i.stop} of string length #{s.length}"
-        seq << s[i.start..i.stop]
-      end
-    else
-      abort "oh dear I appear to have a problem finding #{list[0].chromosome}"
+  list.sort! { |x,y| x.start <=> y.start }
+  seq=""
+  if genome_hash.has_key?(list[0].chromosome)
+    list.each do |i|
+      s = genome_hash[i.chromosome]
+      # puts "substring #{i.start} to #{i.stop} of string length #{s.length}"
+      seq << s[i.start..i.stop]
     end
-    puts ">#{list[0].id}"
-    puts seq
-  else # strand == "-"
-    list.sort! { |x,y| y.stop <=> x.stop }
-    seq=""
-    if genome_hash.has_key?(list[0].chromosome)
-      list.each do |i|
-        s = genome_hash[i.chromosome]
-        seq << s[i.start..i.stop]
-      end
-    else
-      abort "oh dear I appear to have a problem finding #{list[0].chromosome}"
-    end
-    puts ">#{list[0].id}"
-    puts seq.revcomp
+  else
+    abort "oh dear I appear to have a problem finding #{list[0].chromosome}"
   end
+  fasta = ">#{list[0].id}\n"
+  if list[0].strand == "+"
+    fasta << "#{seq}\n"
+  else # strand == "-"
+    fasta << "#{seq.revcomp}\n"
+  end
+  if opts.output
+    out.write(fasta)
+  else
+    puts fasta
+  end
+
 end
